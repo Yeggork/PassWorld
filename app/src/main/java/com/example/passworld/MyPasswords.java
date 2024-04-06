2fd871ee32f6a7386a317ada3fbb175d0e939d8e
Egor <ego210807r@gmail.com>
2024-04-06 11:13:16 +0300

    Fourth+

--- app/src/main/java/com/example/passworld/MyPasswords.java
+++ app/src/main/java/com/example/passworld/MyPasswords.java
@@ -3,8 +3,9 @@ package com.example.passworld;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-public class MyPasswords extends MainActivity{
+public class MyPasswords extends MainActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
